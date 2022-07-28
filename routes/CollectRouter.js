b844ab1470cace0d0163a658ae7f02e1f020c4a0
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2022-07-28 09:03:15 +0530

    minor chnages

--- routes/CollectRouter.js
+++ routes/CollectRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { convertSlangs } = require("../controllers/CovertSlangsController");
-
+//(task-1)get the slang
+//demo: https://localhost:8080/collect/convertslang?word=Welcome&lang=nl
 router.route("/convertslang").get(convertSlangs);
 
 module.exports = router;
